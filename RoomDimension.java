e5cc741dc5a39ad484059f83949f2085792f18f0
alliecatstanz34 <alliecatstanz34@gmail.com>
2020-08-28 16:40:58 -0700

    first draft RoomDimension

--- RoomDimension.java
+++ RoomDimension.java
@@ -1,3 +1,20 @@
 public class RoomDimension {
-  
+    double length;
+    double width;
+
+    //constructs a RoomDimension with the given length and width
+    public RoomDimension (double lengthIn, double widthIn){
+        length = lengthIn;
+        width = widthIn;
+    }
+
+    //returns the area of RoomDimension
+    public double getArea(){
+        return length * width;
+    }
+
+    //returns the length and width of RoomDimension as string 
+    public String toString(){
+        return length + " X " + width;
+    }
 }
