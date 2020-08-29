538052f926c045f8e8d58c4f3369b06913821a3c
alliecatstanz34 <alliecatstanz34@gmail.com>
2020-08-28 20:34:29 -0700

    added to RoomCarpet with tests in Main

--- Main.java
+++ Main.java
@@ -1,3 +1,9 @@
 public class Main {
     
+    public static void main (String [] args){
+        RoomDimension test = new RoomDimension(3,4);
+
+        System.out.println(test.getArea());
+        System.out.println(test.toString());
+    }
 }
--- RoomCarpet.java
+++ RoomCarpet.java
@@ -1,3 +1,21 @@
 public class RoomCarpet {
-    
+
+    RoomDimension size;
+    double carpetCost;
+
+    public RoomCarpet (RoomDimension dim, double cost){
+        size = dim;
+        carpetCost = cost;
+    }
+
+    //returns the total cost of the carpet with the given carpet area
+    //and carpet cost 
+    public double getTotal(){
+        return size.getArea() * carpetCost;
+    }
+
+    //returns _____ as a string 
+    // public String toString(){
+    //     return length + " X " + width;
+    // }
 }
